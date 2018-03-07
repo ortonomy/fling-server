4804bb0127beaa6532ae39fab5e1c31bf574f365
Chan Woo Kim <kim.chanwoo@yahoo.com>
2018-03-07 12:50:13 +0800

    revert

--- db/schema/setup.sql
+++ db/schema/setup.sql
@@ -1052,7 +1052,6 @@ ALTER TABLE IF EXISTS flingapp_custom.user
 -- 5. core freelancer entity
 CREATE TABLE flingapp.freelancer(
   fl_id UUID NOT NULL DEFAULT gen_random_uuid(),
-  fl_shortcode TEXT NOT NULL,
   fl_first_name TEXT NOT NULL DEFAULT 'John',
   fl_last_name TEXT NOT NULL DEFAULT 'Doe',
   fl_is_native_speaker BOOLEAN NOT NULL DEFAULT true,
@@ -1661,16 +1660,6 @@ $$ LANGUAGE sql IMMUTABLE;
 COMMENT ON FUNCTION flingapp_private.is_empty(TEXT) IS 'Find empty strings or strings containing only whitespace';
 
 
--- 4. Generate short URL from name & UUID for freelancer
-CREATE OR REPLACE FUNCTION flingapp_private.gen_freelancer_shortcode()
-  RETURNS trigger language plpgsql AS
-$$
-begin
-  new.fl_shortcode:= CONCAT(lower(new.fl_last_name),'-',lower(new.fl_first_name),'-',SUBSTR(CAST(new.fl_id AS varchar(50)),1,6));
-  return new;
-end 
-$$;
-
 
 
 -- ***** AUTH *****
@@ -2136,12 +2125,8 @@ CREATE TRIGGER proj_updated_at BEFORE UPDATE
   EXECUTE PROCEDURE flingapp_private.set_updated_at();
 
 
-CREATE TRIGGER add_freelancer_gen_shortURL BEFORE INSERT 
-  ON flingapp.freelancer
-  FOR EACH ROW
-  EXECUTE PROCEDURE flingapp_private.gen_freelancer_shortcode();
 
-  
+
 -- **** Privileges
 
 -- SCHEMA GRANTS
